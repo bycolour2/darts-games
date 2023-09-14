6db8177f15f7701ac3af24820b5cb05f8ba0c086
bycolour2 <ruskir7@gmail.com>
2023-09-14 22:32:38 +0300

    :heavy_minus_sign: chore: removed old deps

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./app/App.tsx";
 import "./index.css";
-import { attachReduxDevTools } from "@effector/redux-devtools-adapter";
 import { RouterProvider } from "atomic-router-react";
 import { router } from "./shared/config";
 import { appStarted } from "./shared/config";
-
-// attachReduxDevTools();
 
 appStarted();
 ReactDOM.createRoot(document.getElementById("root")!).render(
