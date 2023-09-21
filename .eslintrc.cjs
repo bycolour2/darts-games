19152eee56e09db89625eb89bf87b7ff81decfaf
bycolour2 <ruskir7@gmail.com>
2023-09-21 23:34:46 +0300

    :technologist: ((eslint)): chore: fixed eslint and added dayjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,8 +5,8 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    // 'plugin:@typescript-eslint/recommended',
+    // 'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:react-hooks/recommended',
     'plugin:effector/future',
     'plugin:effector/patronum',
@@ -20,9 +20,31 @@ module.exports = {
     sourceType: 'module',
     tsconfigRootDir: __dirname,
   },
-  plugins: ['react-refresh', 'prettier', 'effector'],
-  rules: {
-    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-    '@typescript-eslint/no-non-null-assertion': 'off',
-  },
+  plugins: ['react-refresh', 'effector', 'prettier'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'], // Your TypeScript files extension
+
+      // As mentioned in the comments, you should extend TypeScript plugins here,
+      // instead of extending them outside the `overrides`.
+      // If you don't want to extend any rules, you don't need an `extends` attribute.
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      ],
+
+      parserOptions: {
+        project: ['./tsconfig.json'], // Specify it only for TypeScript files
+      },
+      rules: {
+        'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+      },
+    },
+  ],
+  // rules: {
+  //   'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+  //   '@typescript-eslint/no-non-null-assertion': 'off',
+  // },
 };
