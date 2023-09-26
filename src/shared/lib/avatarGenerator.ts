e8258dd7ec0fa410d30c30fcd97aca491a193ee5
bycolour2 <ruskir7@gmail.com>
2023-09-27 00:06:17 +0300

    :ambulance: fix: filename github bug

--- src/shared/lib/avatarGenerator.ts
+++ src/shared/lib/avatarGenerator.ts
@@ -1,6 +1,14 @@
 import { createAvatar } from '@dicebear/core';
 import { openPeeps } from '@dicebear/collection';
 
+export const generateAvatarByName = (seed: string) => {
+  return createAvatar(openPeeps, {
+    seed,
+    size: 128,
+    // ... other options
+  }).toDataUriSync();
+};
+
 const generateRandomString = (length: number) => {
   let result = '';
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
--- src/shared/lib/index.ts
+++ src/shared/lib/index.ts
@@ -1,3 +1,2 @@
 export { cn } from './lib';
-export { generateRandomAvatar } from './generateRandomAvatar';
-export { generateAvatarByName } from './generateAvatarByName';
+export { generateAvatarByName, generateRandomAvatar } from './avatarGenerator';
